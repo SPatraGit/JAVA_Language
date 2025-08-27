a284d44d2993bc8e3e3f66052f66a4b755cc37df
SPatraGit <snehasispatra2027@gmail.com>
2025-08-27 17:51:00 +0530

    commit

---
+++ Basic/Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String a = sc.nextLine();
+        System.out.println("Your Name is "+ a);
+    }
+}
