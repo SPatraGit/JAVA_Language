4ef2d2abd3a7003e6f011f084dd2cebde3ae6088
SPatraGit <snehasispatra2027@gmail.com>
2025-08-27 18:21:16 +0530

    commit

--- String/Index.java
+++ String/Index.java
@@ -1,12 +1,13 @@
 package String;
 
 import java.util.Scanner;
-public class Equal {
+public class Index {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your old name: ");
         String a = sc.nextLine();
-        System.out.println(a.equals("Bubai"));
-        System.out.println(a.equalsIgnoreCase("Bubai"));
+        System.out.println(a.indexOf("a"));
+        System.out.println(a.indexOf("b",1));
+        System.out.println(a.lastIndexOf("b"));
     }
 }
